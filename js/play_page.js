e3793a3d2768996b2e458bfa3a1335953b366f46
NinjaSaph <andra.oica@yahoo.com>
2018-10-29 10:56:15 +0200

    Editted page Play. More divs

--- js/play_page.js
+++ js/play_page.js
@@ -1,24 +1,9 @@
 
-function proceed_login()
+function change_div(div1, div2)
 {
-   d1 = document.getElementById("login_box");
-   d2 = document.getElementById("gametype_box");
-   if( d2.style.display == "none" )
-   {
-      d1.style.display = "none";
-      d2.style.display = "block";
-   }
-   else
-   {
-      d1.style.display = "block";
-      d2.style.display = "none";
-   }
-}
-function proceed_gametype()
-{
-   d1 = document.getElementById("gametype_box");
-   d2 = document.getElementById("gameBox");
-   if( d2.style.display == "none" )
+   d1 = document.getElementById(div1);
+   d2 = document.getElementById(div2);
+   if( d2.style.display = "none" )
    {
       d1.style.display = "none";
       d2.style.display = "block";
