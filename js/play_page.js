0c4eec2b6ed7899bb8d4af253ff750849fb550d6
NinjaSaph <andra.oica@yahoo.com>
2018-10-29 04:36:36 +0200

    Changes on Play page, now working changeable divs

--- js/play_page.js
+++ js/play_page.js
@@ -1,8 +1,23 @@
 
-function changeDiv(div1,div2)
+function proceed_login()
 {
-   d1 = document.getElementById(div1);
-   d2 = document.getElementById(div2);
+   d1 = document.getElementById("login_box");
+   d2 = document.getElementById("gametype_box");
+   if( d2.style.display == "none" )
+   {
+      d1.style.display = "none";
+      d2.style.display = "block";
+   }
+   else
+   {
+      d1.style.display = "block";
+      d2.style.display = "none";
+   }
+}
+function proceed_gametype()
+{
+   d1 = document.getElementById("gametype_box");
+   d2 = document.getElementById("gameBox");
    if( d2.style.display == "none" )
    {
       d1.style.display = "none";
